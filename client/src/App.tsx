ede12099735742d4c81b70834a29b6ee1e4bd1fd
Ruan Portella <ruanmorales29@gmail.com>
2024-11-23 18:29:12 -0300

    Feat: Cria rotas da aplicação

--- client/src/App.tsx
+++
@@ -1,9 +0,0 @@
-
-function App() {
-  return (
-    <div className="bg-red-600 w-[100vw] h-[100vh]">
-    </div>
-  )
-}
-
-export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route } from 'react-router'
+import { Routes } from 'react-router'
+import Rides from './pages/Rides'
+import Trips from './pages/Trips'
+import { Toaster } from '@/components/ui/sonner'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Toaster />
+        <Routes>
+          <Route path="/" element={<Rides />} />
+          <Route path="/viagens" element={<Trips />} />
+        </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )

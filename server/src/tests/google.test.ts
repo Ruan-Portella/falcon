7cb74430c41b1638afea68fac599c7c3abd93e58
Ruan Portella <ruanmorales29@gmail.com>
2024-11-24 23:25:39 -0300

    Feat: Testa Google

---
+++ server/src/tests/google.test.ts
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const fetchMock = require('fetch-mock');
+import GoogleModel from "../models/google.model";
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Driver', () => {
+  beforeEach(() => {
+    sinon.restore();
+    global.fetch = sinon.stub().resolves({
+      json: () => Promise.resolve({ routes: {} }),
+    });
+  });
+
+  it('Testa driver model getDriversFilter', async () => {
+    const googleModel = new GoogleModel();
+    const drivers = await googleModel.getRoute({origin: '', destination: ''});
+
+    expect(drivers).to.have.property('routes');
+  });
+})

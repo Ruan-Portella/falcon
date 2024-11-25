04336aa121af0d8f3f15abb768390c5e30d3b83e
Ruan Portella <ruanmorales29@gmail.com>
2024-11-24 23:24:22 -0300

    Feat: Setup Inicial De Testes Backend

---
+++ server/jest.config.ts
@@ -0,0 +1,28 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.m?jsx?$': 'babel-jest',
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(chai)/)',
+  ],
+};
+
+export default config;
